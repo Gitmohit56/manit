d0c60dce4c3f6c3552b21c55b7557d13cf984789
manit <23781a3133@gmail.com>
2024-10-14 15:46:14 +0530

    initial commit

---
+++ mohit/ch4.java
@@ -0,0 +1,15 @@
+package mohit;
+
+public class ch4 {
+    public static void main(String[] args) {
+        
+    //if else statement
+    int age = 22;
+    if(age>18){
+        System.out.println("yes you drive the car");}
+        else{
+            System.out.println("no you not drive the car");
+        }
+   }
+    
+}
---
+++ mohit/manit.java
@@ -0,0 +1,31 @@
+package mohit;
+
+public class manit {
+    public static void main(String[] args) {
+//     String a = "Har ry";
+//     String value = name.toLowerCase();
+//     String Value = name.toUpperCase(); 
+//     String trim = name.trim();
+//     System.out.println(name.substring(1,3));
+//     System.out.println(name.replace('r','p'));
+//     System.out.println(name.startsWith("Har"));
+//     System.out.println(name.endsWith("ry"));
+//     System.out.println(name.charAt(3));
+//     System.out.println(name.indexOf("ry"));
+//     String modifiedName = "harryrryrry";
+//     System.out.println(modifiedName.indexOf("rry",4));
+//     System.out.println(name.equals("Harry"));
+//     System.out.println(name.equalsIgnoreCase("harry"));
+//     System.out.println("i am mohit kumar \b am a good boy");
+    
+    
+
+
+// System.out.println(name.toLowerCase());
+// System.out.println(name.replace(' ','_'));
+// //problem 4
+// String letter = "dear <|name|>,thanks a lot";
+// letter = letter.replace("<|name|>"," Mohit");
+// System.out.println(letter);
+    }
+}
